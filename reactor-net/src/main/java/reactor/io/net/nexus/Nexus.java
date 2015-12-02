d46c326f98b1580374310d28e89cca24998376b9
Stephane Maldini <smaldini@pivotal.io>
2015-12-02 13:02:20 +0000

    Allow cross origin for the nexus void energy (stream feed)

--- reactor-net/src/main/java/reactor/io/net/nexus/Nexus.java
+++ reactor-net/src/main/java/reactor/io/net/nexus/Nexus.java
@@ -16,21 +16,28 @@
 
 package reactor.io.net.nexus;
 
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.Publishers;
 import reactor.fn.timer.Timer;
 import reactor.io.buffer.Buffer;
 import reactor.io.codec.json.JsonCodec;
 import reactor.io.net.ReactiveChannel;
 import reactor.io.net.ReactiveChannelHandler;
+import reactor.io.net.ReactiveNet;
 import reactor.io.net.ReactivePeer;
 import reactor.io.net.http.HttpChannel;
 import reactor.io.net.http.HttpServer;
+import reactor.io.net.http.model.Headers;
+import reactor.io.net.http.model.HttpHeaders;
 
 /**
  * @author Stephane Maldini
@@ -39,11 +46,39 @@ import reactor.io.net.http.HttpServer;
 public final class Nexus extends ReactivePeer<Buffer, Buffer, ReactiveChannel<Buffer, Buffer>>
 		implements ReactiveChannelHandler<Buffer, Buffer, HttpChannel<Buffer, Buffer>> {
 
+	private static final Logger log = LoggerFactory.getLogger(Nexus.class);
+
 	private static final String API_STREAM_URL = "/nexus/stream";
+	private static final String EXIT_URL       = "/exit";
 
 	private final HttpServer<Buffer, Buffer> server;
 	private final JsonCodec<Map, Map>        jsonCodec;
 	private final Map<String, Object> metrics = new HashMap<>();
+
+	public static void main(String... args) throws Exception {
+		log.info("Deploying Nexus... ");
+
+		Nexus nexus = ReactiveNet.nexus();
+
+		final CountDownLatch stopped = new CountDownLatch(1);
+
+		nexus.server.get(EXIT_URL, new ReactiveChannelHandler<Buffer, Buffer, HttpChannel<Buffer, Buffer>>() {
+			@Override
+			public Publisher<Void> apply(HttpChannel<Buffer, Buffer> channel) {
+				stopped.countDown();
+				return Publishers.empty();
+			}
+		});
+
+		nexus.startAndAwait();
+
+		InetSocketAddress addr = nexus.getServer()
+		                              .getListenAddress();
+		log.info("Nexus Warped, Void Energy available under http://" + addr.getHostName() + ":" + addr.getPort() +
+				API_STREAM_URL);
+
+		stopped.await();
+	}
 
 	/**
 	 *
@@ -53,6 +88,7 @@ public final class Nexus extends ReactivePeer<Buffer, Buffer, ReactiveChannel<Bu
 	public static Nexus create(HttpServer<Buffer, Buffer> server) {
 
 		Nexus nexus = new Nexus(server.getDefaultTimer(), server);
+		log.info("Warping Nexus...");
 
 		server.get(API_STREAM_URL, nexus);
 
@@ -134,7 +170,8 @@ public final class Nexus extends ReactivePeer<Buffer, Buffer, ReactiveChannel<Bu
 
 	@Override
 	public Publisher<Void> apply(HttpChannel<Buffer, Buffer> channel) {
-		return channel.writeWith(jsonCodec.encode(Publishers.just(metrics)));
+		return channel.responseHeader("Access-Control-Allow-Origin", "*")
+		              .writeWith(jsonCodec.encode(Publishers.just(metrics)));
 	}
 
 	public HttpServer<Buffer, Buffer> getServer() {
