c77c3809c83577bd5885b65dda9e909dc31e8370
Alex Petrov <oleksandr.petrov@gmail.com>
2015-12-30 18:00:46 +0100

    Move http tests to http package

--- reactor-net/src/test/java/reactor/io/net/http/ClientServerHttpTests.java
+++ reactor-net/src/test/java/reactor/io/net/http/ClientServerHttpTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package reactor.io.net.tcp;
+package reactor.io.net.http;
 
 import java.util.ArrayList;
 import java.util.Arrays;
