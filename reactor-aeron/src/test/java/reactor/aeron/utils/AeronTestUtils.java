3913cb58117213e787fd98a4d459d2b6d50c783e
Stephane Maldini <smaldini@pivotal.io>
2016-03-11 23:31:58 +0000

    move SocketUtils

--- reactor-aeron/src/test/java/reactor/aeron/utils/AeronTestUtils.java
+++ reactor-aeron/src/test/java/reactor/aeron/utils/AeronTestUtils.java
@@ -16,7 +16,7 @@
 package reactor.aeron.utils;
 
 import reactor.core.test.TestSubscriber;
-import reactor.io.netty.SocketUtils;
+import reactor.io.netty.util.SocketUtils;
 import uk.co.real_logic.aeron.driver.Configuration;
 import uk.co.real_logic.aeron.driver.MediaDriver;
 import uk.co.real_logic.aeron.driver.ThreadingMode;
